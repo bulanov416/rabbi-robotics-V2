a733ebefe4ddd6e8ba44945c9de23bf8910f7623
Levi <legof4@outlook.com>
2017-02-27 21:20:18 -0500

    Debug OpModes and RP Code: 1. Debug Autonomous class written, for sensor output testing, constantly returns EODS values; 2. TeleDebug written for servo value adjustment, adjusts servo values in 0.05 increments using left and right bumpers; 3. RPLWiggle follows left side of line; 4. Hardcode value adjustments; 5. Preset turn added for beacon left-side; 6. Changed to only use Back EODS for inital line detection; 7. Hardcoded right turn and motion; 8. Shooting sequence coded; 9. Second beacon uncommented.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousRPLWiggle.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonomousRPLWiggle.java
@@ -27,16 +27,18 @@ public class AutonomousRPLWiggle extends LinearOpMode {
     Servo button_right;
     Servo wall_servo;
     TouchSensor touch;
+    DcMotor fly;
+    DcMotor intake;
     //Constants
     final float WHEEL_DIAMETER = 10f;
     final float PI = 3.1415f;
-    final float THRESHOLD = 0.3f;
+    final float THRESHOLD = 0.2f;
     final float LEFT_RETRACTED = 0.90f;
     final float LEFT_DEPLOYED = 0.01f;
     final float RIGHT_RETRACTED = 0.92f;
     final float RIGHT_DEPLOYED = 0.15f;
     final float WS_RETRACTED = 0.7f;
-    final float WS_DEPLOYED = 0.2f;
+    final float WS_DEPLOYED = 0.31f;
 
 
     @Override
@@ -48,6 +50,8 @@ public class AutonomousRPLWiggle extends LinearOpMode {
         lb = hardwareMap.dcMotor.get("lb");
         r.setDirection(DcMotor.Direction.REVERSE);
         rb.setDirection(DcMotor.Direction.REVERSE);
+        fly = hardwareMap.dcMotor.get("fly");
+        intake = hardwareMap.dcMotor.get("in");
         //Servos
         button_left = hardwareMap.servo.get("bl");
         button_right = hardwareMap.servo.get("br");
@@ -74,15 +78,29 @@ public class AutonomousRPLWiggle extends LinearOpMode {
 
             pressBeacon();
             if (!opModeIsActive()) break;
-            //Drive back to shoot
-            driveEncoder(-0.8, -73);
-            //INSERT SHOOTING CODE
 
+            //Drive back to shoot
+            wall_servo.setPosition(WS_RETRACTED);
+            fly.setPower(-0.95);
+            driveEncoder(-0.8, -72);
+            //INSERT SHOOTING CODE
+            intake.setPower(-0.95);
+            sleepOpMode(4000);
+            fly.setPower(0);
+            intake.setPower(0);
             //Drive forwards
-            driveEncoder(0.65, 58);
+            drive(0.65);
+            sleepOpMode(850);
+            stopDrive();
             //RIGHT TURN
+            setLeftPower(0.5);
+            setRightPower(-0.5);
+            sleepOpMode(1500);
+            stopDrive();
             //SECOND BEACON
-            //pressBeacon();
+            drive(0.4);
+            sleepOpMode(300);
+            pressBeacon();
             //LEFT TURN, 135 DEGREES
             //DRIVE UNTIL CENTER VORTEX
             /**
@@ -94,7 +112,7 @@ public class AutonomousRPLWiggle extends LinearOpMode {
             /**
             drive(0.3);
             sleepOpMode(350);**/
-
+            break;
         }
         //Closes hardware instances
         stopDrive();
@@ -102,6 +120,8 @@ public class AutonomousRPLWiggle extends LinearOpMode {
         r.close();
         lb.close();
         rb.close();
+        fly.close();
+        intake.close();
         button_left.close();
         button_right.close();
         wall_servo.close();
@@ -128,30 +148,32 @@ public class AutonomousRPLWiggle extends LinearOpMode {
     }
     //CM forward, power forwards
     public void driveEncoder(double power, double cm) throws InterruptedException{
-        lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        long startTime = System.currentTimeMillis();
         r.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         l.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         setMotorModes(DcMotor.RunMode.RUN_TO_POSITION);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
         l.setPower(power);
         r.setPower(power);
-        lb.setPower(power);
-        rb.setPower(power);
+        lb.setPower(0);
+        rb.setPower(0);
         int ticks = (int) (cm * 1120 * 1.35/(PI * WHEEL_DIAMETER));
-        telemetry.addLine("Ticks: " + ticks);
-        telemetry.update();
         l.setTargetPosition(ticks + l.getCurrentPosition());
         r.setTargetPosition(ticks + r.getCurrentPosition());
-        lb.setTargetPosition(ticks + lb.getCurrentPosition());
-        rb.setTargetPosition(ticks + rb.getCurrentPosition());
-        while ((l.isBusy() || r.isBusy() || lb.isBusy() || rb.isBusy()) && opModeIsActive()) {
+        //lb.setTargetPosition(ticks + lb.getCurrentPosition());
+        //rb.setTargetPosition(ticks + rb.getCurrentPosition());
+        while ((l.isBusy() || r.isBusy()) && opModeIsActive()) {
             sleepOpMode(1);
         }
         setMotorModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        lb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rb.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         stopDrive();
     }
     //Degrees clockwise, power absolute value
     public void turnEncoder(double power, double degrees) throws InterruptedException{
+        long startTime = System.currentTimeMillis();
         lb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         r.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -178,7 +200,7 @@ public class AutonomousRPLWiggle extends LinearOpMode {
             lb.setTargetPosition(-ticks + lb.getCurrentPosition());
             rb.setTargetPosition(ticks + rb.getCurrentPosition());
         }
-        while ((l.isBusy() || r.isBusy() || lb.isBusy() || rb.isBusy())&&opModeIsActive()) {
+        while ((l.isBusy() || r.isBusy() || lb.isBusy() || rb.isBusy()) && opModeIsActive() && System.currentTimeMillis() < startTime + 3000) {
             sleepOpMode(1);
         }
         setMotorModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -215,27 +237,38 @@ public class AutonomousRPLWiggle extends LinearOpMode {
         color_left.enableLed(false);
         setMotorModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         //Moves to Line from Start
-        while (eodsFore.getLightDetected() < THRESHOLD && opModeIsActive()) drive(0.4);
-        while (eodsBack.getLightDetected() < THRESHOLD && opModeIsActive()) drive(0.2);
+        /**
+        while (eodsFore.getLightDetected() < THRESHOLD && opModeIsActive()) {
+            drive(0.2);
+            telemetry.addLine("Fore: " + eodsFore.getLightDetected() + "\nBack: " + eodsBack.getLightDetected());
+            telemetry.update();
+
+        }**/
+        while (eodsBack.getLightDetected() < 0.5 && opModeIsActive()) {
+            drive(0.37);
+            telemetry.addLine("Fore: " + eodsFore.getLightDetected() + "\nBack: " + eodsBack.getLightDetected());
+            telemetry.update();
+        }
+        sleepOpMode(125);
         stopDrive();
         sleepOpMode(100);
         if (!opModeIsActive()) return;
         //Turns left onto Line
         while (eodsFore.getLightDetected() < THRESHOLD && opModeIsActive()) {
-            setLeftPower(-0.4);
-            setRightPower(0.4);
+            setLeftPower(-0.38);
+            setRightPower(0.38);
         }
         if (!opModeIsActive()) return;
         stopDrive();
         //Wiggle Line-Follower
         while (!touch.isPressed()) {
             while (eodsFore.getLightDetected() < THRESHOLD && !touch.isPressed() && opModeIsActive()) {
-                setRightPower(0.52);
+                setLeftPower(0.41);
             }
             if (!opModeIsActive()) return;
             stopDrive();
             while (eodsFore.getLightDetected() > THRESHOLD && !touch.isPressed() && opModeIsActive()) {
-                setLeftPower(0.52);
+                setRightPower(0.41);
             }
             if (!opModeIsActive()) return;
             stopDrive();
@@ -257,17 +290,19 @@ public class AutonomousRPLWiggle extends LinearOpMode {
         if (colorFirstSide) { button_left.setPosition(LEFT_DEPLOYED);
         } else { button_right.setPosition(RIGHT_DEPLOYED); }
         //Final Correction
-        if(!colorFirstSide) {
-            this.setLeftPower(-0.2);
-            this.setRightPower(0.2);
+        if(colorFirstSide) {
+            this.setLeftPower(0.2);
+            this.setRightPower(-0.2);
         }
         //Waits for servos to move
-        sleepOpMode(250);
+        sleepOpMode(240);
+        stopDrive();
+        sleepOpMode(400);
         if (!opModeIsActive()) return;
         stopDrive();
         //Drives forward and presses button
-        drive(0.13);
-        sleepOpMode(900);
+        drive(0.17);
+        sleepOpMode(1000);
         //Retracts Servos
         button_left.setPosition(LEFT_RETRACTED);
         button_right.setPosition(RIGHT_RETRACTED);
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Debug.java
@@ -0,0 +1,80 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+
+/**
+ * Created by Levi on 2/27/2017.
+ */
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Debug")
+
+public class Debug extends LinearOpMode{
+
+    DcMotor l;
+    DcMotor r;
+    DcMotor lb;
+    DcMotor rb;
+    OpticalDistanceSensor eodsFore;
+    OpticalDistanceSensor eodsBack;
+    ColorSensor color_left;
+    Servo button_left;
+    Servo button_right;
+    Servo wall_servo;
+    TouchSensor touch;
+    //Constants
+    final float WHEEL_DIAMETER = 10f;
+    final float PI = 3.1415f;
+    final float THRESHOLD = 0.3f;
+    final float LEFT_RETRACTED = 0.90f;
+    final float LEFT_DEPLOYED = 0.01f;
+    final float RIGHT_RETRACTED = 0.92f;
+    final float RIGHT_DEPLOYED = 0.15f;
+    final float WS_RETRACTED = 0.7f;
+    final float WS_DEPLOYED = 0.2f;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Motors
+        l = hardwareMap.dcMotor.get("l");
+        r = hardwareMap.dcMotor.get("r");
+        rb = hardwareMap.dcMotor.get("rb");
+        lb = hardwareMap.dcMotor.get("lb");
+        r.setDirection(DcMotor.Direction.REVERSE);
+        rb.setDirection(DcMotor.Direction.REVERSE);
+        //Servos
+        button_left = hardwareMap.servo.get("bl");
+        button_right = hardwareMap.servo.get("br");
+        wall_servo = hardwareMap.servo.get("ws");
+        wall_servo.setPosition(WS_DEPLOYED);
+        button_left.setPosition(LEFT_RETRACTED);
+        button_right.setPosition(RIGHT_RETRACTED);
+        //Sensors
+        eodsFore = hardwareMap.opticalDistanceSensor.get("eodsF");
+        eodsBack = hardwareMap.opticalDistanceSensor.get("eodsB");
+        eodsBack.enableLed(true);
+        eodsFore.enableLed(true);
+        color_left = hardwareMap.colorSensor.get("cl");
+        touch = hardwareMap.touchSensor.get("t");
+        waitForStart();
+        while (opModeIsActive()) {
+            //Press First Beacon
+            while (opModeIsActive()) {
+                telemetry.addLine("Fore: " + eodsFore.getLightDetected() + "\nBack: " + eodsBack.getLightDetected());
+                telemetry.update();
+                sleepOpMode(5);
+            }
+        }
+    }
+
+    public void sleepOpMode(double millTime) throws InterruptedException {
+        double time = System.currentTimeMillis();
+        while (opModeIsActive() && System.currentTimeMillis() < time + millTime) {
+            this.sleep(1);
+        }
+    }
+
+}
